687aba7c9d97434be1c4ea5cdf7a510d243ab983
EnumaOub <rayane.oubenali@gmail.com>
2024-07-21 00:35:16 +0200

    Refactor Gameboard and Ship and added possibility to attack ships in gameboard

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ class Ship {
     constructor(coordInit, coordFin) {
         this.hitNb = 0;
         this.sunk = false;
-        this.coordInit = coordInit;
-        this.coordFin = coordFin;
         if (coordInit[0] === coordFin[0] && coordInit[1] !== coordFin[1]) {
             this.length = Math.abs(coordInit[1]-coordFin[1]) + 1
             this.position = "VERTICAL";
+            this.coordInit = coordInit[1] < coordFin[1] ? coordInit: coordFin;
+            this.coordFin = coordInit[1] > coordFin[1] ? coordInit: coordFin;
         }
         else if (coordInit[1] === coordFin[1] && coordInit[0] !== coordFin[0]) {
             this.length = Math.abs(coordInit[0]-coordFin[0]) + 1
             this.position = "HORIZONTAL";
+            this.coordInit = coordInit[0] < coordFin[0] ? coordInit: coordFin;
+            this.coordFin = coordInit[0] > coordFin[0] ? coordInit: coordFin;
         }
         else {
             throw new Error(`The coordinate given ${coordInit} and ${coordFin} doesn't allow the creation of a ship`);
@@ -19,12 +21,15 @@ class Ship {
 
     hit() {
         this.hitNb++;
+        this.isSunk();
     }
 
     isSunk() {
         if (this.hitNb === this.length) {
             this.sunk = true;
+            return true;
         }
+        return false;
     }
 };
 
@@ -37,8 +42,39 @@ class Gameboard {
         setCoord.forEach((coord) => this.shipArr.push(new Ship(...coord)))
     };
 
-    receiveAttack(coord) {
+    checkShipAttack(ship, coord) {
+        if (ship.position === "HORIZONTAL" && coord[1] === ship.coordInit[1]) {
+            return coord[0] >= ship.coordInit[0] && coord[0] <= ship.coordFin[0] 
+        }
+        else if (ship.position === "VERTICAL" && coord[0] === ship.coordInit[0]) {
+            return coord[1] >= ship.coordInit[1] && coord[1] <= ship.coordFin[1] 
+        }
+        else {
+            return false
+        }
+    }
 
+    receiveAttack(coord) {
+        let touched = null;
+        
+        for (let i=0; i<this.shipAlive; i++) {
+            if (this.checkShipAttack(this.shipArr[i], coord)) {
+                touched = i;
+            }
+        }
+        this.attackNb++;
+        if (touched !== null) {
+            this.shipArr[touched].hit();
+            if (this.shipArr[touched].sunk) {
+                this.shipArr.splice(touched, 1);
+                this.shipAlive--;
+            }
+            return true;
+        }
+        else {
+            this.attackMissed++;
+            return false;
+        }
     };
 };
 
--- test.js
+++ test.js
@@ -113,3 +113,125 @@ describe("Gameboard init", () => {
         expect(testBoard.shipArr[1].position).toBe("VERTICAL");
     });
 });
+
+describe("Gameboard attack miss", () => {
+    const setCoord = [
+        [[0, 5], [3, 5]],
+        [[2, 2], [2, 0]],
+        [[4, 1], [4, 5]]
+    ]
+    const attackCoord = [0, 0];
+
+    const testBoard = new main.Gameboard(setCoord);
+    const attackRes = testBoard.receiveAttack(attackCoord);
+    
+    test('Check Gameboard attack miss', () => {
+        expect(attackRes).toBe(false);
+    });
+    test('Check Gameboard number of ships', () => {
+        expect(testBoard.shipAlive).toBe(3);
+    });
+    test('Check Gameboard number of attack ', () => {
+        expect(testBoard.attackNb).toBe(1);
+    });
+    test('Check Gameboard number of missed attack', () => {
+        expect(testBoard.attackMissed).toBe(1);
+    });
+    test('Check Gameboard first ship hit param', () => {
+        expect(testBoard.shipArr[0].hitNb).toBe(0);
+    });
+    test('Check Gameboard second ship sunk param', () => {
+        expect(testBoard.shipArr[1].sunk).toBe(false);
+    });
+});
+
+describe("Gameboard attack touch Horizontal", () => {
+    const setCoord = [
+        [[0, 5], [3, 5]],
+        [[2, 2], [2, 0]],
+        [[4, 1], [4, 5]]
+    ]
+    const attackCoord = [0, 5];
+
+    const testBoard = new main.Gameboard(setCoord);
+    const attackRes = testBoard.receiveAttack(attackCoord);
+    
+    test('Check Gameboard attack miss', () => {
+        expect(attackRes).toBe(true);
+    });
+    test('Check Gameboard number of ships', () => {
+        expect(testBoard.shipAlive).toBe(3);
+    });
+    test('Check Gameboard number of attack ', () => {
+        expect(testBoard.attackNb).toBe(1);
+    });
+    test('Check Gameboard number of missed attack', () => {
+        expect(testBoard.attackMissed).toBe(0);
+    });
+    test('Check Gameboard first ship hit param', () => {
+        expect(testBoard.shipArr[0].hitNb).toBe(1);
+    });
+    test('Check Gameboard first ship sunk param', () => {
+        expect(testBoard.shipArr[0].sunk).toBe(false);
+    });
+});
+
+describe("Gameboard attack touch Vertical", () => {
+    const setCoord = [
+        [[0, 5], [3, 5]],
+        [[2, 2], [2, 0]],
+        [[4, 1], [4, 5]]
+    ]
+    const attackCoord = [2, 0];
+
+    const testBoard = new main.Gameboard(setCoord);
+    const attackRes = testBoard.receiveAttack(attackCoord);
+    
+    test('Check Gameboard attack miss', () => {
+        expect(attackRes).toBe(true);
+    });
+    test('Check Gameboard number of ships', () => {
+        expect(testBoard.shipAlive).toBe(3);
+    });
+    test('Check Gameboard number of attack ', () => {
+        expect(testBoard.attackNb).toBe(1);
+    });
+    test('Check Gameboard number of missed attack', () => {
+        expect(testBoard.attackMissed).toBe(0);
+    });
+    test('Check Gameboard second ship hit param', () => {
+        expect(testBoard.shipArr[1].hitNb).toBe(1);
+    });
+    test('Check Gameboard second ship sunk param', () => {
+        expect(testBoard.shipArr[1].sunk).toBe(false);
+    });
+});
+
+describe("Gameboard attack touch sink first", () => {
+    const setCoord = [
+        [[0, 5], [3, 5]],
+        [[2, 2], [2, 0]],
+        [[4, 1], [4, 5]]
+    ]
+
+    const testBoard = new main.Gameboard(setCoord);
+    testBoard.receiveAttack([0, 5]);
+    testBoard.receiveAttack([0, 0]);
+    testBoard.receiveAttack([1, 5]);
+    testBoard.receiveAttack([2, 5]);
+    const attackRes = testBoard.receiveAttack([3, 5]);
+    
+    test('Check Gameboard attack miss', () => {
+        expect(attackRes).toBe(true);
+    });
+    test('Check Gameboard number of ships', () => {
+        expect(testBoard.shipAlive).toBe(2);
+    });
+    test('Check Gameboard number of attack ', () => {
+        expect(testBoard.attackNb).toBe(5);
+    });
+    test('Check Gameboard number of missed attack', () => {
+        expect(testBoard.attackMissed).toBe(1);
+    });
+    console.log(testBoard.shipArr[0])
+});
