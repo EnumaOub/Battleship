ad28c619910bb78046eac572df9695d97aa857e0
EnumaOub <rayane.oubenali@gmail.com>
2024-07-20 23:04:43 +0200

    Create Gameboard class and refactore Ship class with coordinate of the ship

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 class Ship {
-    constructor(length) {
-        this.length = length;
+    constructor(coordInit, coordFin) {
         this.hitNb = 0;
         this.sunk = false;
+        this.coordInit = coordInit;
+        this.coordFin = coordFin;
+        if (coordInit[0] === coordFin[0] && coordInit[1] !== coordFin[1]) {
+            this.length = Math.abs(coordInit[1]-coordFin[1]) + 1
+        }
+        else if (coordInit[1] === coordFin[1] && coordInit[0] !== coordFin[0]) {
+            this.length = Math.abs(coordInit[0]-coordFin[0]) + 1
+        }
+        else {
+            throw new Error(`The coordinate given ${coordInit} and ${coordFin} doesn't allow the creation of a ship`);
+        }
     }
 
     hit() {
@@ -14,8 +24,15 @@ class Ship {
             this.sunk = true;
         }
     }
+};
+
+class Gameboard {
+    constructor(coordInit, coordFin) {
+        this.ship = new Ship(coordInit, coordFin);
+    };
 }
 
 module.exports = {
-    Ship
+    Ship,
+    Gameboard
 };
--- test.js
+++ test.js
@@ -1,16 +1,42 @@
 const main = require("./main");
 
-describe("Ship init", () => {
-    const length = 5;
-    const testShip = new main.Ship(length);
+// Ship 
+
+describe("Ship Error init [0, 1] [0, 1]", () => {
+    const coordInit = [0, 1];
+    const coordFin = [0, 1];
+    expect(() => new main.Ship(coordInit, coordFin)).toThrow(Error(`The coordinate given ${coordInit} and ${coordFin} doesn't allow the creation of a ship`));
+});
+
+describe("Ship init [0, 5] [3, 5]", () => {
+    const coordInit = [0, 5];
+    const coordFin = [3, 5];
+    const testShip = new main.Ship(coordInit, coordFin);
   
     test('Check ship length param', () => {
-        expect(testShip.length).toBe(length);
+        expect(testShip.length).toBe(4);
     });
     test('Check ship hit param init', () => {
         expect(testShip.hitNb).toBe(0);
     });
-    test('Check sunk param init', () => {
+    test('Check ship sunk param init', () => {
+        expect(testShip.sunk).toBe(false);
+    });
+
+});
+
+describe("Ship init [4, 5] [4, 7]", () => {
+    const coordInit = [4, 5];
+    const coordFin = [4, 7];
+    const testShip = new main.Ship(coordInit, coordFin);
+  
+    test('Check ship length param', () => {
+        expect(testShip.length).toBe(3);
+    });
+    test('Check ship hit param init', () => {
+        expect(testShip.hitNb).toBe(0);
+    });
+    test('Check ship sunk param init', () => {
         expect(testShip.sunk).toBe(false);
     });
 
@@ -18,8 +44,9 @@ describe("Ship init", () => {
 
 
 describe("Ship 1 hit", () => {
-    const length = 5;
-    const testShip = new main.Ship(length);
+    const coordInit = [0, 5];
+    const coordFin = [3, 5];
+    const testShip = new main.Ship(coordInit, coordFin);
 
     testShip.hit()
     testShip.isSunk()
@@ -27,14 +54,16 @@ describe("Ship 1 hit", () => {
     test('Check ship hit param after 1 hit', () => {
         expect(testShip.hitNb).toBe(1);
     });
-    test('Check sunk param after 1 hit', () => {
+    test('Check ship sunk param after 1 hit', () => {
         expect(testShip.sunk).toBe(false);
     });
 });
 
 describe("Ship full hits", () => {
-    const length = 5;
-    const testShip = new main.Ship(length);
+    const coordInit = [0, 5];
+    const coordFin = [4, 5];
+    const length = 5
+    const testShip = new main.Ship(coordInit, coordFin);
 
     for(let i=0; i<length; i++) {
         testShip.hit()
@@ -46,5 +75,23 @@ describe("Ship full hits", () => {
     });
     test(`Check ship sunk param after ${length} hit`, () => {
         expect(testShip.sunk).toBe(true);
+    });
+});
+
+//Gameboard
+
+describe("Gameboard init", () => {
+    const coordInit = [0, 5];
+    const coordFin = [3, 5];
+    const testBoard = new main.Gameboard(coordInit, coordFin);
+
+    test('Check Gameboard ship length param', () => {
+        expect(testBoard.ship.length).toBe(4);
+    });
+    test('Check Gameboard ship hit param init', () => {
+        expect(testBoard.ship.hitNb).toBe(0);
+    });
+    test('Check Gameboard ship sunk param init', () => {
+        expect(testBoard.ship.sunk).toBe(false);
     });
 });
