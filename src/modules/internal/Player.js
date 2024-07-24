62cf1e87cc2dd0b8f31f8b011a4e75b1131fa170
EnumaOub <rayane.oubenali@gmail.com>
2024-07-24 21:45:18 +0200

    Add comments to Player and Ship classes

--- src/modules/internal/Player.js
+++ src/modules/internal/Player.js
@@ -2,16 +2,22 @@ import Gameboard from './Gameboard'
 
 export default class Player {
     constructor(name, realP, xSize, ySize) {
+        // Initialisize if the player is a real one
         this.realP = realP;
+        // Name given to the player
         this.name = name;
+        // Is it the current player
         this.active = false;
+        // board of the player
         this.board = new Gameboard(xSize, ySize);
     };
 
+    // modifiy name of the player
     setName(name) {
         this.name = name;
     };
 
+    // toggle the booleen if its the one playing
     toggleActive() {
         this.active= !this.active;
     }
--- src/modules/internal/Ship.js
+++ src/modules/internal/Ship.js
@@ -1,16 +1,25 @@
 export default class Ship {
     constructor(coordInit, coordFin) {
+        // Initialise of times the ship were hit
         this.hitNb = 0;
+        // Boolean showing if the ship was hit in all its coordinates
         this.sunk = false;
+        // Check if the coodinates initial and final of the ship given form a vertical ship
         if (coordInit[0] === coordFin[0] && coordInit[1] !== coordFin[1]) {
+            // init length of the ship
             this.length = Math.abs(coordInit[1]-coordFin[1]) + 1
+            // init posision of the ship
             this.position = "VERTICAL";
+            // store initial and final coordinate in function of its position
             this.coordInit = coordInit[1] < coordFin[1] ? coordInit: coordFin;
             this.coordFin = coordInit[1] > coordFin[1] ? coordInit: coordFin;
         }
         else if (coordInit[1] === coordFin[1] && coordInit[0] !== coordFin[0]) {
+            // init length of the ship
             this.length = Math.abs(coordInit[0]-coordFin[0]) + 1
+            // init posision of the ship
             this.position = "HORIZONTAL";
+            // store initial and final coordinate in function of its position
             this.coordInit = coordInit[0] < coordFin[0] ? coordInit: coordFin;
             this.coordFin = coordInit[0] > coordFin[0] ? coordInit: coordFin;
         }
@@ -19,16 +28,18 @@ export default class Ship {
         }
     }
 
-    hit() {
-        this.hitNb++;
-        this.isSunk();
-    }
-
+    // check if ship has sunk
     isSunk() {
         if (this.hitNb === this.length) {
             this.sunk = true;
             return true;
         }
         return false;
+    }
+
+    // if ship is hit iterate the number of hit and check if ship has sunk
+    hit() {
+        this.hitNb++;
+        this.isSunk();
     }
 };
