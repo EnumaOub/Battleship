1da5c82e093ef481477173218a76bf70fd27f4fd
EnumaOub <rayane.oubenali@gmail.com>
2024-07-24 21:51:49 +0200

    Subdivise css into different files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import './styles/styles.css';
+
+import './styles/header.css';
+import './styles/footer.css';
+import './styles/main.css';
+import './styles/dialog.css';
 import { UI } from './modules/ui/ui';
 console.log("TEST")
 UI();
