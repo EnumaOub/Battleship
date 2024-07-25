0ce4502c4aff02dfb0fa50ef3b3c607d4cc3f847
EnumaOub <rayane.oubenali@gmail.com>
2024-07-25 20:11:33 +0200

    Comment factory function which control the game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-
 import './styles/header.css';
 import './styles/footer.css';
 import './styles/main.css';
 import './styles/dialog.css';
+
 import { UI } from './modules/ui/ui';
+
 console.log("TEST")
 UI();
--- src/modules/ui/ControlGame.js
+++ src/modules/ui/ControlGame.js
@@ -1,9 +1,11 @@
 
-
+// Factory function dealing with the control of the player and the computer
 export const ControlGame = function() {
+    // Give results to the round actually played
     const dealResult = function(result, nbShip, target, player) {
         const containerInfo = document.getElementById("info-r");
         const playerActive = document.getElementById("playerG").textContent
+        // Check if one ship was hit
         if (result) {
             target.textContent = "O";
             target.style["background-color"] = "rgba(0,255,0,.3)"
@@ -22,27 +24,39 @@ export const ControlGame = function() {
         }
     }
 
+    // Function attack of a human player
     const attackC = function(event, player) {
-        let target = event.target;
+        // Get all current ship in order to compare after the attack
         const nbShip = player.board.shipAlive;
+        // Get position on front associated with the coordinate clicked by player
+        let target = event.target;
+        // Check that the coordinate clicked is on the grid
         if (target.className != "grid_elem") return
+        // Extract the coordinate from the id of the grid element
         let [yCoord, xCoord] = target.id.match(/\d+/g);
+        // Attack the player's board given
         const result = player.board.receiveAttack([Number(xCoord), Number(yCoord)]);
+        // Apply the results
         dealResult(result, nbShip, target, player);
         return result;
     };
 
+    // Function attack of a computer
     const attackRandom = function(player) {
-        
+        // Get all current ship in order to compare after the attack
         const nbShip = player.board.shipAlive;
+        // Get all possible moves on the board
         const possibleMove = player.board.possibleMove;
-        const coord = possibleMove[Math.floor(Math.random()*possibleMove.length)];
-        const target = document.getElementById(`r_${coord[1]}_c_${coord[0]}`)
+        // get one random coordinate in all possible moves
+        const coord = possibleMove[Math.floor(Math.random() * possibleMove.length)];
+        // Get position on front associated with the coordinate obtained
+        const target = document.getElementById(`r_${coord[1]}_c_${coord[0]}`);
+        // Attack the player's board given
         const result = player.board.receiveAttack([Number(coord[0]), Number(coord[1])]);
+        // Apply the results
         dealResult(result, nbShip, target, player);
         return result;
-
-    }
+    };
 
     return {
         attackC,
